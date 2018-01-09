981dc3d6744588d5980fcbef347c768dd4de50a0
IgnacioLuciano <iglumu@gmail.com>
2018-01-08 18:39:57 -0800

    HandsOnArray
    Arrays

--- HandsOn/src/com/arrays/handson/Arrays1.java
+++ HandsOn/src/com/arrays/handson/Arrays1.java
@@ -4,7 +4,20 @@ public class Arrays1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+//		String Student1 = "Ignacio";
+//		String Student2 = "Lucas";
+//		String Student3 = "Martina";
+//		String Student4 = "Joaquin";
+//		String Student5 = "Nicolas";
+		
+		String[] students = {"Ignacio","Lucas","Martina","Joaquin","Nicolas"};
+		
+		System.out.println(students[0]);
+		 
+	for (int count = 5; count > 0; count--) {
+		 System.out.println(count);
 
-	}
-
+}
+}
 }

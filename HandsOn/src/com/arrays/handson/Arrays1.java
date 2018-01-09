d02b05ea7ceb52b4aad7547b710b7bef47b02b5a
IgnacioLuciano <iglumu@gmail.com>
2018-01-08 19:33:34 -0800

    More changes
    More

--- HandsOn/src/com/arrays/handson/Arrays1.java
+++ HandsOn/src/com/arrays/handson/Arrays1.java
@@ -4,28 +4,26 @@ public class Arrays1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-//		String Student1 = "Ignacio";
-//		String Student2 = "Lucas";
-//		String Student3 = "Martina";
-//		String Student4 = "Joaquin";
-//		String Student5 = "Nicolas";
-		
-		String[] students = {"Ignacio","Lucas","Martina","Joaquin","Nicolas"};
-		
-		System.out.println(students[0]);
-		 
-	for (int count = 5; count > 0; count--) {
-		 System.out.println(count);
 
+		// String Student1 = "Ignacio";
+		// String Student2 = "Lucas";
+		// String Student3 = "Martina";
+		// String Student4 = "Joaquin";
+		// String Student5 = "Nicolas";
+
+		String[] students = { "Ignacio", "Lucas", "Martina", "Joaquin", "Nicolas" };
+
+		System.out.println(students[0]);
+
+		for (int count = 5; count > 0; count--) {
+			System.out.println(count);
+
+		}
+
+		int start = 1;
+		int end = 5;
+		for (int numbers = start; numbers <= end; numbers += 2) {
+			System.out.println(numbers);
+		}
+	}
 }
-	
-	int start = 1;
-	int end = 5;
-	for (int numbers = start; numbers <= end ; numbers+= 2){
-		if (numbers%2 !=0)
-	           System.out.println(numbers);
-}
-}
-}
-	
